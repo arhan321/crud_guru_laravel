8cf5af26fdb5893ee4147e623455fd29e8b0394b
arhan321 <arhanmcz@gmail.com>
2024-06-16 20:21:55 +0700

    fresh from oven

---
+++ crud_by_djn/database/migrations/2024_06_13_105823_create_gurus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gurus', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->date('birth_dath')->nullable();
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gurus');
+    }
+};
